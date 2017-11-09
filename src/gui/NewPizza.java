dca9a87b49f4ac66e531a7ad36837c217854e057
Chris <christiandstrahl@gmail.com>
2017-11-09 16:53:54 -0500

    GUI 2nd Draft

--- src/gui/NewPizza.java
+++ src/gui/NewPizza.java
@@ -8,19 +8,19 @@ import javax.swing.*;
 public class NewPizza {
     private JTextPane pizzaDetailsTextPane;
     private JPanel panel1;
-    private JRadioButton smallSizeRadioButton;
     private JTextPane costTextPane;
-    private JButton addToOrderButton;
+    private JButton completeOrderButton;
     private JButton cancelButton;
-    private JRadioButton mediumSizeRadioButton;
-    private JRadioButton largeSizeRadioButton;
-    private JRadioButton thinCrustRadioButton;
-    private JRadioButton handTossedCrustRadioButton;
-    private JRadioButton deepDishCrustRadioButton;
     private JCheckBox pepperoniCheckBox;
     private JCheckBox sausageCheckBox;
     private JCheckBox mushroomCheckBox;
     private JCheckBox bellPepperCheckBox;
     private JCheckBox extraCheeseCheckBox;
     private JButton voidButton;
+    private JComboBox pizzaSizeBox;
+    private JComboBox crustTypeBox;
+    private JComboBox drinkTypeBox;
+    private JComboBox drinkSizeBox;
+    private JButton addPizzaButton;
+    private JButton addBeverageButton;
 }
